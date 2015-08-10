9dba93908ebede5b3ee1a83273e4609bb6d94bde
LuK1337 <priv.luk@gmail.com>
2015-08-10 03:44:45 +0200

    baffinlite: Set device model to GT-I9060

--- cm.mk
+++ cm.mk
@@ -12,7 +12,7 @@ PRODUCT_DEVICE := baffinlite
 PRODUCT_NAME := cm_baffinlite
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-baffinlite
+PRODUCT_MODEL := GT-I9060
 
 # Set build fingerprint / ID / Prduct Name ect.
 # PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinvj TARGET_DEVICE=baffin BUILD_FINGERPRINT=samsung/baffinvj/baffin:4.1.2/JZO54K/baffinliteLDCAMC2:user/release-keys PRIVATE_BUILD_DESC="baffinvj-user 4.1.2 JZO54K baffinliteLDCAMC2 release-keys"
