3d71ae7bbe63b250f3f493fef003a1d08c93e88c
LuK1337 <priv.luk@gmail.com>
2015-08-20 14:37:13 +0200

    baffinlite: Provide proper device fingerprint

--- cm.mk
+++ cm.mk
@@ -15,4 +15,4 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9060
 
 # Set build fingerprint / ID / Prduct Name ect.
-# PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinvj TARGET_DEVICE=baffin BUILD_FINGERPRINT=samsung/baffinvj/baffin:4.1.2/JZO54K/baffinliteLDCAMC2:user/release-keys PRIVATE_BUILD_DESC="baffinvj-user 4.1.2 JZO54K baffinliteLDCAMC2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinlitexx TARGET_DEVICE=baffinlite BUILD_FINGERPRINT=samsung/baffinlitexx/baffinlite:4.2.2/JDQ39/I9060XXUAOE1:user/release-keys PRIVATE_BUILD_DESC="baffinlitexx-user 4.2.2 JDQ39 I9060XXUAOE1 release-keys"
