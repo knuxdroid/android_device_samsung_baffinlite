b57055e10c922ee1415ab3ebde396934f45a7121
LuK1337 <priv.luk@gmail.com>
2015-08-20 15:57:46 +0200

    baffinlite: Update proprietary files to I9060XXUAOE1

--- cm.mk
+++ cm.mk
@@ -15,4 +15,4 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9060
 
 # Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinlitexx TARGET_DEVICE=baffinlite BUILD_FINGERPRINT=samsung/baffinlitexx/baffinlite:4.2.2/JDQ39/I9060XXUAOE1:user/release-keys PRIVATE_BUILD_DESC="baffinlitexx-user 4.2.2 JDQ39 I9060XXUAOE1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinlitexx TARGET_DEVICE=baffinlite BUILD_FINGERPRINT=samsung/baffinlitexx/baffinlite:4.2.2/JDQ39/I9060XXUAOG1:user/release-keys PRIVATE_BUILD_DESC="baffinlitexx-user 4.2.2 JDQ39 I9060XXUAOG1 release-keys"
